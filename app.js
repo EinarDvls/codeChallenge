81e72f7466980ecd64472d4f0b0de073210cd7b8
Einar Davalos <99060661+EinarDvls@users.noreply.github.com>
2022-05-04 14:04:31 -0500

    challenge

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const {
+  getStudents,
+  getEmails,
+  getCredits,
+} = require("./lib/controller/StudentController");
+const Reader = require("./lib/utils/Reader");
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+app.get("/", (request, response) => {
+  response.json("Code Challenge LaunchX!");
+});
+
+app.get("/v1/students", (request, response) => {
+  const students = getStudents();
+  response.json(students);
+});
+
+app.get("/v1/students/email", (request, response) => {
+  const students = getStudents();
+  const emails = getEmails(students);
+  response.json(emails);
+});
+
+app.get("/v1/students/credits", (request, response) => {
+  const students = getStudents();
+  const credits = getCredits(students);
+  response.json(credits);
+});
+
+app.listen(port, () => {
+  console.log(`Student API in localhost:${port}`);
+});
